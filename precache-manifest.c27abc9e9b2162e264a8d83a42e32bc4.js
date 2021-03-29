aa717f68be119403e49149d5b230fbc8222d68ba
Kaian Ferreira dos Santos <kaian@risistemas.com.br>
2021-03-29 07:27:32 -0300

    deploy

---
+++ precache-manifest.c27abc9e9b2162e264a8d83a42e32bc4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1214c029ca688ef58c",
+    "url": "/Tweet-Tracker/css/about.6b909caf.css"
+  },
+  {
+    "revision": "45ea24ed69a4febefc43",
+    "url": "/Tweet-Tracker/css/app.22d721dc.css"
+  },
+  {
+    "revision": "a6e5229e1bf3b61efbf981b9b4e3a115",
+    "url": "/Tweet-Tracker/fonts/Bungee-Regular.a6e5229e.ttf"
+  },
+  {
+    "revision": "44169c432f6bc9521bdd4074971376f7",
+    "url": "/Tweet-Tracker/fonts/Chivo-Bold.44169c43.ttf"
+  },
+  {
+    "revision": "f2b6049b9d103746abc359eb4b480826",
+    "url": "/Tweet-Tracker/fonts/Chivo-Light.f2b6049b.ttf"
+  },
+  {
+    "revision": "41bd4374ac5ea9f57b746aa61ce98923",
+    "url": "/Tweet-Tracker/img/pin.41bd4374.svg"
+  },
+  {
+    "revision": "9a0523dd05006300d5a9c7ad0e07a5f8",
+    "url": "/Tweet-Tracker/index.html"
+  },
+  {
+    "revision": "df1214c029ca688ef58c",
+    "url": "/Tweet-Tracker/js/about.8ba2e666.js"
+  },
+  {
+    "revision": "45ea24ed69a4febefc43",
+    "url": "/Tweet-Tracker/js/app.97eb39b3.js"
+  },
+  {
+    "revision": "618024c6ee1cb48b6797",
+    "url": "/Tweet-Tracker/js/chunk-vendors.e1a22cf8.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/Tweet-Tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Tweet-Tracker/robots.txt"
+  }
+]);
