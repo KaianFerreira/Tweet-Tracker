91651ac23d1803158fb895fc86ae62147a86e511
Kaian Ferreira dos Santos <kaian@risistemas.com.br>
2021-03-29 05:30:23 -0300

    frontend | Homepage and map

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div id="app">
-    <div id="map" :class="{'open': openMenu}"></div>
-    <router-view class="actions" :class="{'open': openMenu}"/>
+    <div id="map"></div>
+    <div class="search" :class="{'open': openMenu}">
+      <div class="input-group" v-if="user">
+        <input @keyup.enter="searchHashtag" @blur="searchHashtag" v-model="search" placeholder="Pesquisar hashtag" type="text">
+        <IconSearch class="icon"/>
+      </div>
+    </div>
+    <Menu v-if="user" :switchMenu="switchMenu" />
+    <router-view  class="actions" :class="{'open': openMenu}"/>
   </div>
 </template>
 <script>
-import { setToken } from './libs/util'
+import { mapState } from 'vuex'
+import { getLocationsByHashTag } from './api/tweet'
+import IconSearch from './components/icons/Search'
+import Menu from './components/Menu'
+import { setToken, getToken } from './libs/util'
 export default {
+  components: {
+    IconSearch,
+    Menu
+  },
+  computed: {
+    ...mapState(['user', 'openMenu'])
+  },
   data () {
     return {
-      openMenu: true,
+      search: '',
       locations: [],
       map: null
     }
@@ -21,8 +39,8 @@ export default {
           iconUrl: require('./assets/img/pin.svg'),
           iconSize: [38, 95]
         })
+        // pin and popup with the hashtag text
         const markers = this.locations.map(x => {
-          console.log(x)
           return window.L.marker([JSON.stringify(x.cordenates.coords[1]), JSON.stringify(x.cordenates.coords[0])], { icon }).addTo(this.map)
             .bindPopup(`
               <div class="popup-map">
@@ -35,30 +53,42 @@ export default {
       }
     }
   },
+  // solving problems with map size
   created () {
     window.addEventListener('resize', this.resize)
   },
   destroyed () {
     window.removeEventListener('resize', this.resize)
   },
-  mounted () {
-    console.log(this.$route)
+  async mounted () {
+    // verifiy if is a callback url
     if (this.$route.query.oauth_token) {
-      setToken(this.$route.query)
+      if (!getToken()) await setToken(this.$route.query)
+      this.$router.push('/')
+      this.$store.commit('user', getToken())
     }
+
+    // genenerating the map
     this.map = window.L.map('map', {
       zoomControl: false
     }).setView([51.505, -0.09], 13)
     window.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map)
-    window.L.control.zoom({
-      position: 'bottomright'
-    }).addTo(this.map)
+    // window.L.control.zoom({
+    //   position: 'bottomright'
+    // }).addTo(this.map)
   },
   methods: {
     resize () {
       this.map.invalidateSize()
+    },
+    async searchHashtag () {
+      this.locations = await getLocationsByHashTag(this.search)
+    },
+    // open and close menu
+    switchMenu () {
+      this.$store.commit('openMenu', !this.openMenu)
     }
   }
 }
@@ -72,6 +102,35 @@ export default {
 
 <style lang="scss" scoped>
 @import "./assets/scss/_variables";
+.search {
+  transition: 0.3s;
+  z-index: 9999;
+  position: absolute;
+  // left: 15vw;
+  display: flex;
+  width: calc(100vw - 20px);
+  margin: 10px;
+  &.open {
+    display: none;
+  }
+  .input-group {
+    display: flex;
+    background-color: #fff;
+    padding: 15px;
+    width: 100%;
+    border-radius: 14px;
+    input {
+      width: 100%;
+      font-size: 14px;
+      border: none;
+      outline: none;
+      font-family: chivo;
+    }
+    .icon {
+      width: 20px;
+    }
+  }
+}
 #map {
   transition: 0.3s;
   position: absolute;
@@ -84,15 +143,11 @@ export default {
     left: 100% ;
   }
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 .actions {
+  position: absolute;
+  z-index: 9999;
+  overflow: hidden;
   transition: 0.3s;
   width: 0;
   height: 100%;
@@ -104,5 +159,16 @@ export default {
 @media screen and (min-width: $width-desktop) {
   #map { &.open { left: 300px !important; } }
   .actions { &.open { width: 300px !important; } }
+  .search {
+    justify-content: flex-start;
+    left: 15vw;
+    &.open {
+      display: flex;
+      left: 40vw;
+    }
+    .input-group {
+      width: 500px;
+    }
+  }
 }
 </style>
---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,78 @@
+<template>
+  <section>
+    <div class="content" :class="{ 'open' : openMenu }">
+      <div @click="switchMenu()">
+        <IconHamburguer css="icon hamburger"/>
+      </div>
+      <div @click="$router.push('/')">
+        <IconHome css="icon home"/>
+      </div>
+      <div>
+        <IconUser css="icon user"/>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import IconHome from './icons/Home'
+import IconHamburguer from './icons/Hamburguer'
+import IconUser from './icons/User'
+import { mapState } from 'vuex'
+export default {
+  components: {
+    IconHome,
+    IconHamburguer,
+    IconUser
+  },
+  computed: {
+    ...mapState(['openMenu'])
+  },
+  methods: {
+    switchMenu () {
+      this.$store.commit('openMenu', !this.openMenu)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/_variables";
+  .content {
+    z-index: 9999;
+    background-color: #fff;
+    bottom: 0;
+    position: absolute;
+    width: calc(100% - 40px);
+    height: 45px;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 20px;
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .icon {
+      width: 30px;
+      fill: #000;
+    }
+  }
+  @media screen and (min-width: $width-desktop) {
+    .home, .user { display: none; }
+    .content {
+      transition: 0.3s;
+      cursor: pointer;
+      justify-content: center;
+      width: 25px;
+      height: 25px;
+      padding: 10px;
+      border-radius: 14px;
+      top: 10px;
+      left: 10%;
+      &.open {
+        left: 35vw;
+      }
+    }
+  }
+</style>
